d646a6ebb420331b2ad64129a8e246b2f4826256
Wilkie Olin-Ammentorp <wolinamm@gmail.com>
2024-01-11 16:14:31 -0600

    adding src from old project

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,35 @@
-using PhasorNetworks
-using Test
+using Lux, MLUtils, MLDatasets, OneHotArrays, Statistics, PhasorNetworks, Test
+using Random: Xoshiro
+using Base: @kwdef
+using Zygote: withgradient
+using Optimisers, ComponentArrays
+using Statistics: mean
+using LinearAlgebra: diag
+using PhasorNetworks: bind
+
+#global args for all tests
+n_x = 101
+n_y = 101
+n_vsa = 1
+epsilon = 0.10
+repeats = 6
+epsilon = 0.02
+spk_args = default_spk_args()
+tspan = (0.0, repeats*1.0)
+tbase = collect(tspan[1]:spk_args.dt:tspan[2])
+
+@kwdef mutable struct Args
+    η::Float64 = 3e-4       ## learning rate
+    batchsize::Int = 256    ## batch size
+    epochs::Int = 4        ## number of epochs
+    use_cuda::Bool = false   ## use gpu (if cuda available)
+    rng::Xoshiro = Xoshiro(42) ## global rng
+end
+
+include("vsa_tests.jl")
+include("network_tests.jl")
 
 @testset "PhasorNetworks.jl" begin
-    # Write your tests here.
+    vsa_tests()
+    network_tests()
 end
