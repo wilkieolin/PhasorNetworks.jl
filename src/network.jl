4cd5fad9bcc7535e0bf769aa4eb1ffb0b79cb628
Wilkie Olin-Ammentorp <wolinamm@gmail.com>
2024-01-18 18:01:53 -0600

    updating layer call

--- src/network.jl
+++ src/network.jl
@@ -17,7 +17,7 @@ function MakeSpiking(spk_args::SpikingArgs, repeats::Int)
     return MakeSpiking(spk_args, repeats, (0.0, 1.0 * repeats), 0.0)
 end
 
-function (a::MakeSpiking)(x::AbstractArray)
+function (a::MakeSpiking)(x::AbstractArray, params::LuxParams, state::NamedTuple)
     train = phase_to_train(x, a.spk_args, repeats = a.repeats, offset = a.offset)
     call = SpikingCall(train, a.spk_args, a.tspan)
     return call
