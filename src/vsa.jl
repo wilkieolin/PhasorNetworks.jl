8cc39801c84d88d2d747f2d1f4b5130ac91250d5
Wilkie Olin-Ammentorp <wolinamm@gmail.com>
2024-08-19 22:58:26 -0500

    enabling backprop through phasorResonant spiking parameters

--- src/vsa.jl
+++ src/vsa.jl
@@ -140,21 +140,32 @@ function v_bundle_project(x::LocalCurrent, w::AbstractArray{<:Real,2}, b::Abstra
     return next_call
 end
 
-function v_bundle_project(x::LocalCurrent, params; tspan::Tuple{<:Real, <:Real}, spk_args::SpikingArgs, return_solution::Bool=false)
+function v_bundle_project(x::LocalCurrent, params; tspan::Tuple{<:Real, <:Real}, return_solution::Bool=false)
     #set up functions to define the neuron's differential equations
-    angular_frequency = 2 * pi / spk_args.t_period
-    k = (spk_args.leakage + 1im * angular_frequency)
     output_shape = (size(params.weight, 1), x.shape[2])
     #make the initial potential the bias value
     u0 = zeros(ComplexF32, output_shape)
     #shift the solver span by the function's time offset
     tspan = tspan .+ x.offset
     #enable bias if used
+    
     if haskey(params, :bias_real) && haskey(params, :bias_imag)
-        dzdt(u, p, t) = k .* u + p.weight * x.current_fn(t) .+ bias_current(p.bias_real .+ 1im .* p.bias_imag, t, x.offset, spk_args)
+        function dzdt(u, p, t)
+            angular_frequency = 2 * pi / p.t_period[1]
+            k = (p.leakage[1] + 1im * angular_frequency)
+            du = k .* u + p.weight * x.current_fn(t) .+ bias_current(p.bias_real .+ 1im .* p.bias_imag, t, x.offset, spk_args)
+            return du
+        end
+
         prob = ODEProblem(dzdt, u0, tspan, params)
     else
-        dzdt_nobias(u, p, t) = k .* u + p.weight * x.current_fn(t)
+        function dzdt_nobias(u, p, t)
+            angular_frequency = 2 * pi / p.t_period[1]
+            k = (p.leakage[1] + 1im * angular_frequency)
+            du = k .* u + p.weight * x.current_fn(t)
+            return du
+        end
+
         prob = ODEProblem(dzdt_nobias, u0, tspan, params)
     end
     
